96d56f9973da380a2b164e6b90793e394f72ab60
Jennifer Blau <jblau@bitwerx.com>
2020-08-08 11:57:36 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace LoadFoxProDBToSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DBFLabel = new System.Windows.Forms.Label();
+            this.dbfPath = new System.Windows.Forms.TextBox();
+            this.selectFolder = new System.Windows.Forms.Button();
+            this.sqlServerLabel = new System.Windows.Forms.Label();
+            this.sqlServerName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sqlUserName = new System.Windows.Forms.TextBox();
+            this.sqlPasswordLabel = new System.Windows.Forms.Label();
+            this.sqlPassword = new System.Windows.Forms.TextBox();
+            this.sqlDBLabel = new System.Windows.Forms.Label();
+            this.newSQLDBName = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.lbMessages = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // DBFLabel
+            // 
+            this.DBFLabel.AutoSize = true;
+            this.DBFLabel.Location = new System.Drawing.Point(14, 30);
+            this.DBFLabel.Name = "DBFLabel";
+            this.DBFLabel.Size = new System.Drawing.Size(142, 25);
+            this.DBFLabel.TabIndex = 0;
+            this.DBFLabel.Text = "DBF Location";
+            // 
+            // dbfPath
+            // 
+            this.dbfPath.Location = new System.Drawing.Point(237, 30);
+            this.dbfPath.Name = "dbfPath";
+            this.dbfPath.Size = new System.Drawing.Size(224, 31);
+            this.dbfPath.TabIndex = 1;
+            this.dbfPath.Text = "C:\\Data\\HonoreKin\\AD2014\\DBFF";
+            // 
+            // selectFolder
+            // 
+            this.selectFolder.Location = new System.Drawing.Point(520, 30);
+            this.selectFolder.Name = "selectFolder";
+            this.selectFolder.Size = new System.Drawing.Size(120, 31);
+            this.selectFolder.TabIndex = 2;
+            this.selectFolder.Text = "Select Folder";
+            this.selectFolder.UseVisualStyleBackColor = true;
+            this.selectFolder.Click += new System.EventHandler(this.selectFolder_Click);
+            // 
+            // sqlServerLabel
+            // 
+            this.sqlServerLabel.AutoSize = true;
+            this.sqlServerLabel.Location = new System.Drawing.Point(14, 91);
+            this.sqlServerLabel.Name = "sqlServerLabel";
+            this.sqlServerLabel.Size = new System.Drawing.Size(123, 25);
+            this.sqlServerLabel.TabIndex = 3;
+            this.sqlServerLabel.Text = "SQL Server";
+            // 
+            // sqlServerName
+            // 
+            this.sqlServerName.Location = new System.Drawing.Point(237, 88);
+            this.sqlServerName.Name = "sqlServerName";
+            this.sqlServerName.Size = new System.Drawing.Size(224, 31);
+            this.sqlServerName.TabIndex = 4;
+            this.sqlServerName.Text = "SQL1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "SQL User";
+            // 
+            // sqlUserName
+            // 
+            this.sqlUserName.Location = new System.Drawing.Point(237, 140);
+            this.sqlUserName.Name = "sqlUserName";
+            this.sqlUserName.Size = new System.Drawing.Size(224, 31);
+            this.sqlUserName.TabIndex = 6;
+            this.sqlUserName.Text = "SA";
+            // 
+            // sqlPasswordLabel
+            // 
+            this.sqlPasswordLabel.AutoSize = true;
+            this.sqlPasswordLabel.Location = new System.Drawing.Point(14, 193);
+            this.sqlPasswordLabel.Name = "sqlPasswordLabel";
+            this.sqlPasswordLabel.Size = new System.Drawing.Size(154, 25);
+            this.sqlPasswordLabel.TabIndex = 7;
+            this.sqlPasswordLabel.Text = "SQL Password";
+            // 
+            // sqlPassword
+            // 
+            this.sqlPassword.Location = new System.Drawing.Point(237, 190);
+            this.sqlPassword.Name = "sqlPassword";
+            this.sqlPassword.Size = new System.Drawing.Size(224, 31);
+            this.sqlPassword.TabIndex = 8;
+            this.sqlPassword.Text = "z6@A*U23Fg7w";
+            this.sqlPassword.UseSystemPasswordChar = true;
+            // 
+            // sqlDBLabel
+            // 
+            this.sqlDBLabel.AutoSize = true;
+            this.sqlDBLabel.Location = new System.Drawing.Point(12, 244);
+            this.sqlDBLabel.Name = "sqlDBLabel";
+            this.sqlDBLabel.Size = new System.Drawing.Size(199, 25);
+            this.sqlDBLabel.TabIndex = 9;
+            this.sqlDBLabel.Text = "New SQL DB Name";
+            // 
+            // newSQLDBName
+            // 
+            this.newSQLDBName.Location = new System.Drawing.Point(237, 244);
+            this.newSQLDBName.Name = "newSQLDBName";
+            this.newSQLDBName.Size = new System.Drawing.Size(224, 31);
+            this.newSQLDBName.TabIndex = 10;
+            this.newSQLDBName.Text = "HonoreKin_AD20";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(520, 227);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(165, 48);
+            this.startButton.TabIndex = 11;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // lbMessages
+            // 
+            this.lbMessages.FormattingEnabled = true;
+            this.lbMessages.ItemHeight = 25;
+            this.lbMessages.Location = new System.Drawing.Point(19, 340);
+            this.lbMessages.Name = "lbMessages";
+            this.lbMessages.Size = new System.Drawing.Size(675, 229);
+            this.lbMessages.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 629);
+            this.Controls.Add(this.lbMessages);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.newSQLDBName);
+            this.Controls.Add(this.sqlDBLabel);
+            this.Controls.Add(this.sqlPassword);
+            this.Controls.Add(this.sqlPasswordLabel);
+            this.Controls.Add(this.sqlUserName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sqlServerName);
+            this.Controls.Add(this.sqlServerLabel);
+            this.Controls.Add(this.selectFolder);
+            this.Controls.Add(this.dbfPath);
+            this.Controls.Add(this.DBFLabel);
+            this.Name = "Form1";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DBFLabel;
+        private System.Windows.Forms.TextBox dbfPath;
+        private System.Windows.Forms.Button selectFolder;
+        private System.Windows.Forms.Label sqlServerLabel;
+        private System.Windows.Forms.TextBox sqlServerName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sqlUserName;
+        private System.Windows.Forms.Label sqlPasswordLabel;
+        private System.Windows.Forms.TextBox sqlPassword;
+        private System.Windows.Forms.Label sqlDBLabel;
+        private System.Windows.Forms.TextBox newSQLDBName;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ListBox lbMessages;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoadFoxProDBToSQL
+{
+    public partial class Form1 : Form
+    {
+        string _path;
+        SqlConnection _sqlConnection;
+        string _masterConnString;
+        string _newConnString;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void selectFolder_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            string path = null;
+            FolderBrowserDialog pathFileDialog = new FolderBrowserDialog();
+            DialogResult result = pathFileDialog.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                path = pathFileDialog.SelectedPath.ToString();
+                _path = path;
+                dbfPath.Text = path;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            LoadDBF();
+        }
+
+        private void LoadDBF()
+        {
+
+            Stopwatch stopwatch = new Stopwatch();
+            Stopwatch stopwatch2 = new Stopwatch();
+            stopwatch.Start();
+            lbMessages.Items.Add(@"Processing started at {DateTime.Now}");
+            var masterConnString = $"Server = {sqlServerName.Text}; Database = master; User Id ={sqlUserName.Text}; Password ={sqlPassword.Text};";
+            _masterConnString = masterConnString;
+            _newConnString = $"Server ={sqlServerName.Text}; Database = {newSQLDBName.Text}; User Id ={sqlUserName.Text}; Password ={sqlPassword.Text};";
+            bool sqlSuccess = ConnectAndCreateSQLDB(_masterConnString);
+
+            var tableList = ConnectToDBFGetTableList();
+
+
+
+
+        }
+
+        private bool ConnectAndCreateSQLDB(string connString)
+        {
+            bool success = false; 
+            //connect to SQL First
+            SqlConnection sqlConn = new SqlConnection(connString);
+            _sqlConnection = sqlConn;
+            try
+            {
+                sqlConn.Open();
+
+            }
+            catch (Exception ex)
+            {
+                lbMessages.Items.Add($"Cannot open connection to {sqlServerName.Text}. Ex: {ex.Message}");
+            }
+            try
+            {
+                //Create the DB to house the DB data
+                SqlCommand sqlCreateTableCmd = new SqlCommand($"CREATE DATABASE {newSQLDBName.Text.Trim()}", sqlConn);
+                sqlCreateTableCmd.ExecuteNonQuery();
+                success = true;
+            }
+            catch (Exception ex)
+            {
+                lbMessages.Items.Add($"Error occurred creating new DB {newSQLDBName.Text}. Ex:{ex.Message}");
+
+            }
+            sqlConn.Close();
+            _newConnString = $"Server ={sqlServerName.Text}; Database = {newSQLDBName.Text}; User Id ={sqlUserName.Text}; Password ={sqlPassword.Text};";
+            sqlConn.ConnectionString = _newConnString;
+            //make sure we can connect to the new db
+            sqlConn.Open();
+            sqlConn.Close();
+
+            return success;
+        }
+
+        private DataTable ConnectToDBFGetTableList()
+        {
+            var dbConnString = $"Provider = vfpoledb; Data Source = {dbfPath.Text}; Collating Sequence = general;";
+            OleDbConnection ole = new OleDbConnection(dbConnString);
+
+            ole.Open();
+            DataTable tables = ole.GetSchema("Tables");
+            //DataTable columns = ole.GetSchema("Columns");
+
+            foreach (var t in tables.AsEnumerable())
+            {
+                try
+                {
+                    var tableName = t[2].ToString();
+                    DataTable columns = ole.GetSchema("Columns", new[] { null, null, tableName });
+
+                    OleDbCommand comm = ole.CreateCommand();
+                    comm.CommandText = BuildSelectStatement(columns, tableName);
+                    comm.CommandType = CommandType.Text;
+                    var dataReader = comm.ExecuteReader();
+                    DataTable tableData = new DataTable();
+                    tableData.Load(dataReader);
+                    tableData.TableName = tableName;
+
+                    CreateSqlServerTable(columns, _newConnString, tableName);
+                    InsertSqlServerData(tableData, _newConnString);
+                }
+                catch(Exception ex)
+                {
+                    var tableName = t[2].ToString();
+                    lbMessages.Items.Add($"Error occurred on table {tableName}. Exception: {ex.Message}");
+                }
+            }
+
+            return tables;
+        }
+
+        private void CreateSqlServerTable(DataTable dataTable, string connString, string tableName)
+        {
+            lbMessages.Items.Add("Create table Started for Table: {dataTable.TableName}");
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = connString;
+            conn.Open();
+            string createSql = "";
+            try
+            {
+                using (conn)
+                {
+                    createSql = BuildCreateStatement(dataTable, tableName);
+                    SqlCommand createCmd = conn.CreateCommand();
+                    createCmd.CommandText = createSql;
+                    createCmd.ExecuteNonQuery();
+                    createCmd.Dispose();
+                }
+            }
+            catch (SqlException ex)
+            {
+                lbMessages.Items.Add("Sql Exception:" + ex.Message + ", Stack Trace:" + ex.StackTrace + " for tablename: " + tableName + " sql statement " + createSql);
+            }
+            catch (Exception ex2)
+            {
+                lbMessages.Items.Add("Create Table Error:" + ex2.Message + ", Stack Trace:" + ex2.StackTrace + " for tablename: " + tableName + " sql statement " + createSql);
+            }
+            conn.Close();
+            conn.Dispose();
+            lbMessages.Items.Add($"Table {tableName} created successfully.");
+        }
+        private void InsertSqlServerData(DataTable dataTable, string connString)
+        {
+            lbMessages.Items.Add("Insert Data started for Table: " + dataTable.TableName);
+
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            using (conn)
+            {
+                using (dataTable)
+                {
+                    string sqlSelect = String.Format("SELECT * FROM {0}", dataTable);
+
+                    SqlBulkCopy bulkCopy = new SqlBulkCopy(conn);
+                    using (bulkCopy)
+                    {
+                        try
+                        {
+
+                            if (conn.State == ConnectionState.Closed)
+                                conn.Open();
+                            bulkCopy.DestinationTableName = dataTable.TableName;
+                            bulkCopy.BatchSize = 1000;
+                            bulkCopy.BulkCopyTimeout = 360;
+                            bulkCopy.WriteToServer(dataTable);
+
+                            lbMessages.Items.Add("Insert Data Completed Successfully for Table: " + dataTable.TableName);
+                        }
+                        catch (SqlException ex)
+                        {
+                            lbMessages.Items.Add("SQL Exception:" + ex.Message + ", Stack Trace:" + ex.StackTrace);
+                        }
+                        catch (Exception ex2)
+                        {
+                            lbMessages.Items.Add("Exception:" + ex2.Message + ", Stack Trace:" + ex2.StackTrace);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static string BuildCreateStatement(DataTable dataTable, string tableName)
+        {
+
+            int i = 1;
+            string sqlColumnCreate = "";
+            foreach (var row in dataTable.AsEnumerable())
+            {
+                var columName = row[3].ToString();
+                var dataTypeString = row[11].ToString();
+                var maxLength = row[13].ToString();
+                var precision = row[15].ToString();
+                var dataType = GetDataType(dataTypeString,maxLength, precision);
+
+                string primKey = "";
+                if (i == 1)
+                {
+                        primKey = " PRIMARY KEY";
+                        sqlColumnCreate = "CREATE TABLE " + tableName + " ( srcId INT PRIMARY KEY IDENTITY (1, 1), [" + columName.ToString().Trim() + "] " + dataType + ", ";
+                }
+                else if (i == dataTable.Rows.Count)
+                    sqlColumnCreate = sqlColumnCreate + " [" + columName.ToString().Trim() + "] " + dataType + "); ";
+                else
+                    sqlColumnCreate = sqlColumnCreate + " [" + columName.ToString().Trim() + "] " + dataType + ", ";
+                i++;
+            }
+            return sqlColumnCreate;
+        }
+
+        private static string BuildSelectStatement(DataTable dataTable, string tableName)
+        {
+
+            int i = 1;
+            string sqlSelect = "";
+            foreach (var row in dataTable.AsEnumerable())
+            {
+                var columName = row[3].ToString();
+                var dataTypeString = row[11].ToString();
+                var maxLength = row[13].ToString();
+                var precision = row[15].ToString();
+                var dataType = GetDataType(dataTypeString, maxLength, precision);
+
+                string primKey = "";
+                if (i == 1)
+                {
+                    sqlSelect = $"Select  cast(null as integer) as srcId, Cast( {columName.ToString().Trim()} as {dataType.ToString()}) as {columName.ToString()} , ";
+                }
+                else if (i == dataTable.Rows.Count)
+                    sqlSelect = sqlSelect + $" Cast({columName.ToString().Trim()} as {dataType}) as {columName.ToString()} FROM {tableName.ToString()}";
+                else
+                    sqlSelect = sqlSelect + $" Cast({columName.ToString().Trim()} as {dataType} ) as {columName.ToString()}, ";
+                i++;
+            }
+            return sqlSelect;
+        }
+        private static string GetDataType(string dataType, string maxLength, string precision = "0")
+        {
+            //254 is the max length of a column on FoxPro
+            
+            switch(dataType)
+            {
+                case "129":
+                    dataType = $"Varchar(250)";
+                    break;
+                case "133":
+                    //dataType = "Date";
+                    dataType = $"Varchar(254)";
+                    break;
+                case "20":
+                    dataType = "Integer";
+                    break;
+                case "128":
+                    dataType = "Varbinary";
+                    break;
+                case "11":
+                    dataType = "char(1)";
+                    break;
+                case "8":
+                    dataType = $"nvarchar(254)";
+                    break;
+                case "6":
+                    dataType = "decimal(19, 2)";
+                    break;
+                case "7":
+                    dataType = "Datetime";
+                    break;
+                case "134":
+                    dataType = "time";
+                    break;
+                case "135":
+                    dataType = "DateTime";
+                    break;
+                case "14":
+                    dataType = "Decimal(19, 4)";
+                    break;
+                case "5":
+                    dataType = "Decimal(19,2)";
+                    break;
+                case "72":
+                    dataType = $"varchar(254)";
+                    break;
+                default:
+                    dataType = "varchar(254)";
+                    break;
+            };
+
+            return dataType;
+
+        }
+    }
+}
